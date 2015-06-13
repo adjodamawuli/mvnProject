844c3e1867a8c81be9e13eb82f462f40392f3045
KoffiMawuli <adjodalefort@gmail.com>
2015-06-13 13:11:07 +0200

    Signed-off-by: KoffiMawuli <adjodalefort@gmail.com>

---
+++ mvnProject/src/main/java/com/esprit/Controller.java
@@ -0,0 +1,13 @@
+package com.esprit;
+
+public class Controller {
+
+	public Controller() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello world !");
+	}
+
+}
